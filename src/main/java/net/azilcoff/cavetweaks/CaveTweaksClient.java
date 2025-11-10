46b56d45bccdc175c4e0aefa4d7a10b6fde6df1c
azilcoff <azizgenshin&@gmail.com>
2025-11-10 15:43:51 +0300

    null

---
+++ src/main/java/net/azilcoff/cavetweaks/CaveTweaksClient.java
@@ -0,0 +1,10 @@
+package net.azilcoff.cavetweaks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CaveTweaksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
